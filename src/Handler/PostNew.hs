f783b84548d20c59fc273c4b55a61b92ddcb733e
ringo-apo <ringo1625@gmail.com>
2020-10-15 09:17:02 +0900

    20201015

---
+++ src/Handler/PostNew.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE NoImplicitPrelude #-}  --追加
+{-# LANGUAGE OverloadedStrings #-}  --追加
+{-# LANGUAGE TemplateHaskell #-}  --追加
+{-# LANGUAGE MultiParamTypeClasses #-}  --追加
+{-# LANGUAGE TypeFamilies #-}  --追加
+
+module Handler.PostNew where
+
+import Import
+import Yesod.Form.Bootstrap3
+import Text.Markdown (Markdown)
+import Yesod.Text.Markdown
+
+data BlogPost = BlogPost
+    { title :: Text
+    , article :: Markdown
+    }
+
+blogPostForm :: AForm Handler BlogPost
+blogPostForm = BlogPost
+    <$> areq textField (bfs ("Title" :: Text)) Nothing
+    <*> areq markdownField (bfs ("Article" :: Text)) Nothing
+
+
+
+getPostNewR :: Handler Html
+--getPostNewR = error "Not yet implemented: getPostNewR"
+
+getPostNewR = do
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
+    defaultLayout $ do
+        $(widgetFile "posts/new")
